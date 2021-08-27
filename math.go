0632dee3d0cebfca1a1d78e13c0af7d4117d5f4c
decco05 <douglasdecco@gmail.com>
2021-08-27 08:46:30 -0300

    Fix test bug

--- math.go
+++ math.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println(soma(1,19));
+  fmt.Println(Soma(1,19));
 }
 
-func soma(a int, b int) int{
+func Soma(a int, b int) int{
 	return a + b;
 }
